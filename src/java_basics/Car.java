7e13eafaf56ca02c670b0f782d0180f184cb06de
buddhadev <buddhadevmore20@gmail.com>
2026-08-26 01:15:18 +0530

    commit by X

--- src/java_basics/Car.java
+++ src/java_basics/Car.java
@@ -19,6 +19,8 @@ public class Car {
         run c = new run("BMW", 2027);
         System.out.println(c.brand + c.year);
         
-        System.out.println("changes made");
+        System.out.println("Y changes made");
+        
+        System.out.println("X changes made");
     }
 }
